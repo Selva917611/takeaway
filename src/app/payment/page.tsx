37ce4983f30b1a09c3048084b173997ab34b147b
Selva Ganesh <selva9176@gmail.com>
2025-04-16 06:53:45 +0000

    Error: The default export is not a React Component in "/payment/page"     at errorMissingDefaultExport (file:///home/user/studio/node_modules/next/dist/compiled/next-server/app-page.runtime.dev.js:439:9197)     at createComponentTreeInternal (file:///home/user/studio/node_modules/next/dist/compiled/next-server/app-page.runtime.dev.js:439:17214)     at async (file:///home/user/studio/node_modules/next/dist/compiled/next-server/app-page.runtime.dev.js:439:19354)     at async createComponentTreeInternal (file:///home/user/studio/node_modules/next/dist/compiled/next-server/app-page.runtime.dev.js:439:18746)     at async (file:///home/user/studio/node_modules/next/dist/compiled/next-server/app-page.runtime.dev.js:439:19354)     at async createComponentTreeInternal (file:///home/user/studio/node_modules/next/dist/compiled/next-server/app-page.runtime.dev.js:439:18746)     at async getRSCPayload (file:///home/user/studio/node_modules/next/dist/compiled/next-server/app-page.runtime.dev.js:443:9560)     at async renderToStream (file:///home/user/studio/node_modules/next/dist/compiled/next-server/app-page.runtime.dev.js:443:39377)     at async renderToHTMLOrFlightImpl (file:///home/user/studio/node_modules/next/dist/compiled/next-server/app-page.runtime.dev.js:443:27540)     at async doRender (file:///home/user/studio/node_modules/next/dist/server/base-server.js:1650:34)     at async DevServer.renderToResponseWithComponentsImpl (file:///home/user/studio/node_modules/next/dist/server/base-server.js:1915:28)     at async DevServer.renderPageComponent (file:///home/user/studio/node_modules/next/dist/server/base-server.js:2393:24)     at async DevServer.renderToResponseImpl (file:///home/user/studio/node_modules/next/dist/server/base-server.js:2430:32)     at async DevServer.pipeImpl (file:///home/user/studio/node_modules/next/dist/server/base-server.js:1003:25)     at async NextNodeServer.handleCatchallRenderRequest (file:///home/user/studio/node_modules/next/dist/server/next-server.js:304:17)     at async DevServer.handleRequestImpl (file:///home/user/studio/node_modules/next/dist/server/base-server.js:895:17)     at async (file:///home/user/studio/node_modules/next/dist/server/dev/next-dev-server.js:371:20)     at async Span.traceAsyncFn (file:///home/user/studio/node_modules/next/dist/trace/trace.js:157:20)     at async DevServer.handleRequest (file:///home/user/studio/node_modules/next/dist/server/dev/next-dev-server.js:368:24)     at async invokeRender (file:///home/user/studio/node_modules/next/dist/server/lib/router-server.js:247:21)     at async handleRequest (file:///home/user/studio/node_modules/next/dist/server/lib/router-server.js:438:24)     at async requestHandlerImpl (file:///home/user/studio/node_modules/next/dist/server/lib/router-server.js:462:13)     at async Server.requestListener (file:///home/user/studio/node_modules/next/dist/server/lib/start-server.js:158:13)

---
+++ src/app/payment/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+
+const PaymentPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Payment Gateway Integration</h1>
+      <p>This is a placeholder for the UPI payment gateway integration.</p>
+    </div>
+  );
+};
+
+export default PaymentPage;
